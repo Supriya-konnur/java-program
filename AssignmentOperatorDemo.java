b5bc70590e61e0159e04f6696a509b5cfb0970c5
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-10-29 19:35:36 +0530

    Add files via upload
    assignmentoperator

---
+++ AssignmentOperatorDemo.java
@@ -0,0 +1,19 @@
+class AssignmentOperatorDemo{
+
+public static void main(String args[]){
+
+int a=10, b=50, c=30, d=40, e=60;
+a+=20;
+b-=20;
+c*=20;
+d/=20;
+e%=20;
+
+System.out.println("a:" +a);
+System.out.println("b:" +b);
+System.out.println("c:" +c);
+System.out.println("d:" +d);
+System.out.println("e:" +e);
+
+}
+}
