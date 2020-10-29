391359f11cf2336eaac008dad1b59d4bbd42b646
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-10-29 20:04:28 +0530

    Add files via upload
    if_else

---
+++ IfElseDemo.java
@@ -0,0 +1,20 @@
+class IfElseDemo{
+
+public static void main(String args[]){
+
+System.out.println("main started...!");
+
+int age = 18;
+
+if(age > 18){
+
+System.out.println("you are eligible to vote...!");
+}
+else{
+System.out.println("you are not eligible to vote...!");
+}
+
+System.out.println("main ended...!");
+
+}
+}
