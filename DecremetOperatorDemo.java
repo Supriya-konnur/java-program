20abd96c53806a3b63c33247f63bccb0f985d3e6
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-10-29 19:22:08 +0530

    Add files via upload
    unaryoperator

---
+++ DecremetOperatorDemo.java
@@ -0,0 +1,17 @@
+class DecremetOperatorDemo{
+
+public static void main(String args[]){
+
+int x=100;
+int y=200;
+int z=--x;
+
+System.out.println(z--);
+System.out.println(--x);
+System.out.println(y--);
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+
+}
+}
