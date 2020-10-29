8bbbc43c336a94b6b80791c98386c2382e156bd8
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-10-29 19:56:31 +0530

    Add files via upload
    relationaloperator

---
+++ RelationaloperatorDemo.java
@@ -0,0 +1,15 @@
+class RelationaloperatorDemo{
+
+public static void main(String[] args){
+
+int x=50, y=60, z=70;
+
+System.out.println(" x == y :" + (x == y));
+System.out.println(" x != y :" + (x != y));
+System.out.println(" x < y :" + (x < y));
+System.out.println(" y <= z :" + (y <= z ));
+System.out.println(" y > z :" + (y > z));
+System.out.println(" y >= z :" + (y >= z));
+
+}
+}
