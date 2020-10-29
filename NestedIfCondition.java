e7f8125c7943a6e2f3eabac566157dccb62dc274
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-10-29 20:33:27 +0530

    Add files via upload
    nestedif_condition

---
+++ NestedIfCondition.java
@@ -0,0 +1,15 @@
+class NestedIfCondition{
+
+public static void main(String args[]){
+
+int num = 30;
+if(num > 0 && num < 100){
+System.out.println(" The given number present between 0 - 100");
+
+  if(num > 0 && num < 50){
+  System.out.println(" The given number present between 0 - 50");
+  }
+}
+}
+}
+
