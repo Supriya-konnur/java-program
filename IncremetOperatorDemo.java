c61b0ec0a1707ccf3f5fa169690bb6a7201de6aa
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-10-29 19:20:06 +0530

    Add files via upload
    incrementoperator program

---
+++ IncremetOperatorDemo.java
@@ -0,0 +1,17 @@
+class IncremetOperatorDemo{
+
+public static void main(String args[]){
+
+int p=5;
+int q=4;
+int r=q++;
+
+System.out.println(r++);
+System.out.println(++q);
+System.out.println(p++);
+System.out.println(r);
+System.out.println(q);
+System.out.println(p);
+
+}
+}
