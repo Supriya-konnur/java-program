3031e757cd63e7c01ec8f9c0bc4ef3221b246667
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-10-29 20:02:49 +0530

    Add files via upload
    controlflow_statement(ifconditin)

---
+++ IfconditonDemo.java
@@ -0,0 +1,17 @@
+class IfconditonDemo{
+
+public static void main(String args[]){
+
+System.out.println("main started...!");
+
+int age = 18;
+
+if(age == 18){
+
+System.out.println("my age is 18");
+}
+
+System.out.println("main ended...!");
+
+}
+}
