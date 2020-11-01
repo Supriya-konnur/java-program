b74f93307ba913b469ca88ab092c85e944748f0d
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-11-01 20:56:29 +0530

    Add files via upload
    breakstatement

---
+++ BreakstatementDemo.java
@@ -0,0 +1,16 @@
+class BreakstatementDemo{
+public static void main(String[] args){
+
+System.out.println("main started...!");
+
+for(int i=0; i<5;i++){
+
+if(i==3){
+
+System.out.println("breking the loop...at 3");
+break;
+}
+
+}
+}
+}
