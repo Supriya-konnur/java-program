47db01d904f88988f82017333df29051a75412de
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-11-01 20:57:36 +0530

    Add files via upload
    continuestatement

---
+++ ContinuestatementDemo.java
@@ -0,0 +1,14 @@
+class ContinuestatementDemo{
+public static void main(String[] args){
+
+System.out.println("even numbers between 1-10");
+
+for(int i=1; i<10;i++){
+
+if(i%2 == 1){
+continue;
+}
+System.out.println(+i);
+}
+}
+}
